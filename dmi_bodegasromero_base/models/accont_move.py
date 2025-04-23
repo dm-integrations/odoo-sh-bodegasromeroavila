c608f7b92ea0f5ef603acd4fb62f76c71203d8e7
Carlos Ramos <cramosh84@gmail.com>
2025-04-23 11:05:59 +0200

    [ADD] Cambio control de CIF

--- dmi_bodegasromero_base/models/accont_move.py
+++ dmi_bodegasromero_base/models/accont_move.py
@@ -6,7 +6,7 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     def action_post(self):
-        if not self.partner_id.vat:
+        if self.partner_id and not self.partner_id.vat:
             raise UserError(_('El NIF del cliente no es correcto. Por favor verifique el NIF del cliente.'))
 
         return super(AccountMove, self).action_post()
